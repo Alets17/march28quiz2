c343d413a3f185362f8b660d1a5060b3eaf73973
Cruz Ceja <cruzcejaa@student.douglascollege.ca>
2024-03-28 18:16:31 -0700

    Third

--- index.js
+++ index.js
@@ -10,19 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
 
-URI = "mongodb+srv://cruzcejaa:data@cluster0.ehfotai.mongodb.net/Winter24"
 
-mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => {
-  console.log('Connected to MongoDB');
-  // Start your Express server once connected to MongoDB
-  app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-  });
-})
-.catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
+
 
 // Create a Schema object
 
@@ -34,13 +23,35 @@ app.get('/', (req, res) => {
 
 app.post('/', async (req, res) => {
   // get the data from the form
-
+  uri = req.body.myuri
+  console.log(uri)
   // connect to the database and log the connection
-
+  mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Start your Express server once connected to MongoDB
+    
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
   // add the data to the database
+  const name = "Alejandro Cruz Ceja 2"
+  const studentID = 300353501
 
+  const newW24students = new w24students({
+    name,
+    studentID
+  })
+
+  newW24students
+    .save()
+    .then(() => res.json("Student Added!"))
+    .catch((err) => res.status(400).json("Error:" +err))
   // send a response to the user
   res.send(`<h1>Document  Added</h1>`);
 });
 
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
